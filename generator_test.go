1b7344258ed1b88c410284484386c0bd254d06a1
obsti8383 <github@florian-probst.de>
2022-03-05 10:51:22 +0100

    fix go.mod; first version of tests

--- generator_test.go
+++ generator_test.go
@@ -1,41 +1,71 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"strings"
 	"testing"
-
-	"github.com/carlmjohnson/csv"
 )
 
-func TestHelloName(t *testing.T) {
-	alleIdentitaeten := generate(20, "test.test", 5)
-	fmt.Println(alleIdentitaeten)
-	t.Fatal("work in progress")
+func TestStandardCaseWithManagers(t *testing.T) {
+	testIdentityCount := 2000
+	testManagerCount := 1000
+	allIdentities := generate(testIdentityCount, "test.test", testManagerCount)
+
+	// check counts and duplicates
+	countIdentities := len(allIdentities)
+	countManagers := 0
+	countNonManagers := 0
+	countDuplicateEmails := 0
+	emailDuplicates := make(map[string]bool)
+	for _, identity := range allIdentities {
+		if identity.Manager == "" {
+			countManagers++
+		} else {
+			countNonManagers++
+		}
+		if emailDuplicates[identity.Email] {
+			countDuplicateEmails++
+		}
+		emailDuplicates[identity.Email] = true
+	}
+	if countDuplicateEmails > 0 {
+		t.Error(countDuplicateEmails, "duplicate emails. this should only happen in very large datasets. check randum number generator")
+	}
+	if countIdentities != testIdentityCount {
+		t.Error("mismatch in number of identities, should be", testIdentityCount, ", are:", countIdentities)
+	}
+	if countManagers != testManagerCount {
+		t.Error("mismatch in number of managers, should be ", testManagerCount, ", are:", countManagers)
+	}
+	if countNonManagers != testIdentityCount-testManagerCount {
+		t.Error("mismatch in number of non-managers, should be", testIdentityCount-testManagerCount, ", are:", countNonManagers)
+	}
 }
 
-func ExampleFieldReader_options() {
-	in := `first_name;last_name;username
-"Rob";"Pike";rob
-# lines beginning with a # character are ignored
-Ken;Thompson;ken
-"Robert";"Griesemer";"gri"
-`
-	r := csv.NewFieldReader(strings.NewReader(in))
-	r.Comma = ';'
-	r.Comment = '#'
+func TestStandardCaseWithoutManagers(t *testing.T) {
+	testIdentityCount := 2000
+	testManagerCount := 0
+	allIdentities := generate(testIdentityCount, "test.test", testManagerCount)
 
-	for r.Scan() {
-		fmt.Println(r.Field("username"))
+	// check counts and duplicates
+	countIdentities := len(allIdentities)
+	countFilledManagerFields := 0
+	countDuplicateEmails := 0
+	emailDuplicates := make(map[string]bool)
+	for _, identity := range allIdentities {
+		if identity.Manager != "" {
+			countFilledManagerFields++
+		}
+		if emailDuplicates[identity.Email] {
+			countDuplicateEmails++
+		}
+		emailDuplicates[identity.Email] = true
 	}
-
-	if err := r.Err(); err != nil {
-		log.Fatal(err)
+	if countDuplicateEmails > 0 {
+		t.Error(countDuplicateEmails, "duplicate emails. this should only happen in very large datasets. check randum number generator")
 	}
-
-	// Output:
-	// rob
-	// ken
-	// gri
+	if countIdentities != testIdentityCount {
+		t.Error("mismatch in number of identities, should be", testIdentityCount, ", are:", countIdentities)
+	}
+	if countFilledManagerFields > 0 {
+		t.Error("manager fields are not all empty,", countFilledManagerFields, " are filled")
+	}
 }
